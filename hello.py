a4f4be8fd8e36af5cbb56a8e81630bab5ff69bf4
Wacoder Forever <sethomondi088@gmail.com>
2024-07-07 04:14:04 +0300

    sdjkdsfgf

--- hello.py
+++ hello.py
@@ -11,15 +11,15 @@ app.config['DEBUG']=True
 
 @app.route('/')
 def main():
-    return render_template('index.html',current_time=datetime.utcnow())
+    return render_template('index.html',current_time=datetime.utcnow(),_external=True)
 
 @app.route('/user/<string:name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html',name=name,_external=True)
 
 @app.route('/invalid')
 def not_found():
-    return render_template('404.html')
+    return render_template('404.html',_external=True)
 
 if __name__=='__main__':
-    app.run(port=5013)
+    app.run(port=5015)
