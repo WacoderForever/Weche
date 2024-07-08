6fcc758d6d3e99129b438c6c3032bab0b51aff71
Wacoder Forever <sethomondi088@gmail.com>
2024-07-08 18:26:50 +0300

    wtf_forms

--- hello.py
+++ hello.py
@@ -2,16 +2,29 @@ from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import validators,PasswordField,StringField,SubmitField,BooleanField,IntegerField,DateField,DateTimeField
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 
 app.config['DEBUG']=True
+app.config["WTF_CSRF_ENABLED"]=False
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name=StringField("What is your name?:",[validators.DataRequired(),validators.Length(min=2,max=20)])
+    submit=SubmitField("Submit")
+
+@app.route('/',methods=['GET','POST'])
 def main():
-    return render_template('index.html',current_time=datetime.utcnow(),_external=True)
+    name=''
+    form=NameForm()
+
+    if form.validate():
+        name=form.name.data
+
+    return render_template('index.html',current_time=datetime.utcnow(),_external=True,name=name,form=form)
 
 @app.route('/user/<string:name>')
 def user(name):
