5a786bbffa9dd25c5ac309a5399c23b43377d9e1
Wacoder Forever <sethomondi088@gmail.com>
2024-07-09 04:54:16 +0300

    hfghfg

--- hello.py
+++ hello.py
@@ -28,7 +28,7 @@ def index():
     if form.validate_on_submit():
         old_name=session.get('name')
         if old_name is not None and old_name != form.name.data:
-            flash("Seems like you have changed your name")
+            flash("Looks like you have changed your name!")
         session['name'] = form.name.data
         form.name.data=''
         return redirect(url_for('index'))
