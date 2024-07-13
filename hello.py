f12edc990fd843276353f5545ad34c7dc41e18bb
Wacoder Forever <sethomondi088@gmail.com>
2024-07-14 01:14:17 +0300

    ghjghjg

--- hello.py
+++ hello.py
@@ -1,73 +1,71 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, url_for, redirect, session ,flash
+from flask import Flask, render_template, url_for, redirect, session, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_mail import Message
+from flask_mail import Mail, Message
 from threading import Thread
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','default_secret_key')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-app.config['MAIL_SERVER']='smtp.googlemail.com'
-app.config['PORT']= 587
-app.config['MAIL_USE_TLS']=True
-app.config['MAIL_USERNAME']=os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD']=os.getenv('MAIL_PASSWORD')
-app.config['FLASK_MAIL_SUBJECTPREFIX']='[Weche]'
-app.config['FLASKY_MAIL_SENDER']='Flasky Admin <sethomondi7055@gmail.com>'
-app.config['FLASKY_ADMIN']=os.getenv('FLASKY_ADMIN')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['FLASK_MAIL_SUBJECT_PREFIX'] = '[Weche]'
+app.config['FLASK_MAIL_SENDER'] = 'Flasky Admin <sethomondi7055@gmail.com>'
+app.config['FLASKY_ADMIN'] = os.getenv('FLASKY_ADMIN')
 
 csrf = CSRFProtect(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db=SQLAlchemy(app)
-mail=Mail(app)
-msg=Message(app)
+db = SQLAlchemy(app)
+mail = Mail(app)
 
-def send_async_mail():
+def send_async_mail(msg):
     with app.app_context():
         mail.send(msg)
 
-def send_mail(to,subject,template,**kwargs):
-    msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
-                sender=app.config['FLASK_MAIL_SENDER'],recipients=[to])
-    msg.body=render_template(template + '.txt',**kwargs)
-    msg.html=render_template(template + '.html',**kwargs)
-    thr=Thread(target=send_async_mail,args=[app,msg])
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASK_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASK_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_mail, args=[msg])
     thr.start()
     return thr
 
 class Role(db.Model):
-    __tablename__='role'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(20),unique=True)
-    users=db.Relationship('User',backref='role',lazy='dynamic')
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
 class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64),unique=True,index=True)
-    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
-    def __repr(self):
+    def __repr__(self):
         return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
-    name = StringField("What is your name?",validators=[DataRequired(),Length(min=2,max=20)])
+    name = StringField("What is your name?", validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField("Submit")
 
 with app.app_context():
@@ -77,30 +75,30 @@ with app.app_context():
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name=session.get('name')
-        user=User.query.filter_by(username=form.name.data).first()
+        old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
 
         if user is None:
-            user=User(username=form.name.data)
+            user = User(username=form.name.data)
             db.session.add(user)
             db.session.commit()
-            session['known']=False
+            session['known'] = False
 
             if app.config['FLASKY_ADMIN']:
-                send_mail(app.config['FLASKY_ADMIN'],'New user','mail/new_user',user=user)
+                send_mail(app.config['FLASKY_ADMIN'], 'New user', 'mail/new_user', user=user)
         else:
-            session['known']=True
+            session['known'] = True
 
         session['name'] = form.name.data
-        form.name.data=''
+        form.name.data = ''
         return redirect(url_for('index'))
     else:
-        session['name']=session.get('name','')
+        session['name'] = session.get('name', '')
 
     return render_template('index.html', current_time=datetime.utcnow(), name=session.get('name'),
-                            form=form,known=session.get('known',False))
+                           form=form, known=session.get('known', False))
 
 @app.route('/user/<string:name>')
 def user(name):
