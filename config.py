f2fb8dcf3fb67b04dcfda795e188e10cb6141d00
Wacoder Forever <sethomondi088@gmail.com>
2024-07-16 01:34:09 +0300

    hjfgdfgd

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-security-key'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Weche]'
+    FLASKY_MAIL_SENDER = 'Weche Admin <weche@example.com>'
+    FLASKY_ADMIN = os.environ.get('WECHE_ADMIN')
 
     @staticmethod
     def init_app(app):
