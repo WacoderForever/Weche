3d5f996d95a560aef47ddca33abdea0572cc333f
Wacoder Forever <sethomondi088@gmail.com>
2024-07-17 23:56:55 +0300

    fgfdgdfg

--- app/models.py
+++ app/models.py
@@ -16,6 +16,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    password_hash=db.Column(db.Strng(128))
+
+    @password.setter
+    def password(self,password):
+        password_hash=generate_password_hash(password,method="sha265")
+    
 
     def __repr__(self):
         return '<User %r>' % self.username
