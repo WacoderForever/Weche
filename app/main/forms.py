634e8b440f7237d89006ea8dffcaea51bbea0fed
Wacoder Forever <sethomondi088@gmail.com>
2024-07-20 01:24:57 +0300

    gffggd

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms import StringField,SubmitField,PasswordField,BooleanField
+from wtforms.validators import DataRequired,Length,Email
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    email=StringField("Email",validators=[DataRequired(), Length(min=1,max=64),Email()])
+    password=StringField("Password",validators=[DataRequired(),Length(min=8,max=40)])
+    remember_me=BooleanField()
+    submit=SubmitField("Login")
