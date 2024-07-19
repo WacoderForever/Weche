ff82b7d1897d887ce77e5a0c7a11eadcd5a7eb38
Wacoder Forever <sethomondi088@gmail.com>
2024-07-20 01:28:08 +0300

    cfgds

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import auth
+from app.main.forms import LoginForm
 
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html')
+    form=LoginForm()
+    return render_template('auth/login.html',form=form)
