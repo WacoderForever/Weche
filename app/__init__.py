a304be6013de5ffcd9747e1bf02a0ee2eb30f8af
Wacoder Forever <sethomondi088@gmail.com>
2024-07-21 00:31:08 +0300

    fghgfgfg

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -20,6 +23,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
