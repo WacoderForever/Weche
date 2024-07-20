46ab510dfa077b0eed7c3236de91024a399b5cc2
Wacoder Forever <sethomondi088@gmail.com>
2024-07-21 02:27:51 +0300

    hgjhj

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from app.main.forms import LoginForm
 from app.models import User
@@ -11,7 +11,13 @@ def login():
         user=User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user,form.remember_me.data)
-            return redirect(request.args.get('next'),'main.index')
+            return redirect(request.args.get('next') or 'main.index')
         flash('Incorrect email or password')
 
     return render_template('auth/login.html',form=form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for('main.index'))
