e6f245e33ceecf7ca8e3221d16a429a43e24ce75
Wacoder Forever <sethomondi088@gmail.com>
2024-07-21 05:48:33 +0300

    hfghf

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,PasswordField,BooleanField
 from wtforms.validators import DataRequired,Length,Email
+from wtforms import ValidationError
+from app.models import User
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired(), Length(min=2, max=20)])
@@ -11,3 +13,7 @@ class LoginForm(FlaskForm):
     password=PasswordField("Password",validators=[DataRequired(),Length(min=8,max=40)])
     remember_me=BooleanField()
     submit=SubmitField("Login")
+
+    def validate_email(self,field):
+        if not User.query.filter_by(email=field.data).first():
+            raise ValidationError("Account does not exist yet. Create a new account")
