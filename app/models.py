c07562601fac9af9f8181a093b24d372a6cada19
Wacoder Forever <sethomondi088@gmail.com>
2024-07-22 01:47:19 +0300

    hgfhg

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-from itsdangerous import TimedJsonWebSignatureSerializer as Serializer
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 
 @login_manager.user_loader
