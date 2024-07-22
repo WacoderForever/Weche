2fb58e0e885c7d8a5b57a804589873b14d4fea2e
Wacoder Forever <sethomondi088@gmail.com>
2024-07-22 17:22:50 +0300

    hfhg

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from . import mail
 from flask_mail import Message
+from flask import current_app,render_template
 
 def send_async_email(app,message):
     with app.app_context():
@@ -9,4 +10,9 @@ def send_async_email(app,message):
 def send_email(to,subject,template,**kwargs):
     app=current_app._get_current_object()
     msg=Message(app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' '+ subject,
-                sender=app.config['WECHE_MAIL_SENDER'])
+                sender=app.config['WECHE_MAIL_SENDER'],recipients=[to])
+    msg.body=render_template(template +'.txt',**kwargs)
+    msg.html=render_template(template + '.html',**kwargs)
+    thr=Thread(target=send_async_email,args=[app,msg])
+    thr.start()
+    return thr
