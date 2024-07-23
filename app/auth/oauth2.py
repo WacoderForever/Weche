a3267cf208adf49f1081b1e60ae1b2de38b2ddc6
Wacoder Forever <sethomondi088@gmail.com>
2024-07-24 00:58:08 +0300

    fggffd

---
+++ app/auth/oauth2.py
@@ -0,0 +1,14 @@
+ 
+import os  
+from google.oauth2 import service_account  
+from google.auth.transport.requests import Request  
+  
+creds = service_account.Credentials.from_service_account_file(
+    'service_account_key.json',
+    scopes=['https://mail.google.com/']  
+)  
+  
+def get_authenticated_client():  
+    transport = Request()  
+    creds.refresh_token = transport.refresh_token  
+    return creds  
--- app/email.py
+++ app/email.py
@@ -2,12 +2,14 @@ from threading import Thread
 from . import mail
 from flask_mail import Message
 from flask import current_app,render_template
+from .auth.oauth2 import get_authenticated_client
 
 def send_async_email(app,message):
     with app.app_context():
         mail.send(message)
 
 def send_email(to,subject,template,**kwargs):
+    creds = get_authenticated_client()
     app=current_app._get_current_object()
     msg=Message(app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' '+ subject,
                 sender=app.config['WECHE_MAIL_SENDER'],recipients=[to])
