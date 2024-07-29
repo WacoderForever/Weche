0faf5b2ba08fb742262452e57aafc48da36baa6f
Wacoder Forever <sethomondi088@gmail.com>
2024-07-30 02:27:48 +0300

    fghgf

--- app/auth/oauth2.py
+++ app/auth/oauth2.py
@@ -4,15 +4,21 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from google.auth import exceptions
 
-# Load the service account key
-creds = service_account.Credentials.from_service_account_file(
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
-    scopes=['https://www.googleapis.com/auth/gmail.send']
-)
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SERVICE_ACCOUNT_FILE = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 def get_authenticated_client():
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=SCOPES
+    )
     try:
         creds.refresh(Request())
     except exceptions.RefreshError:
         raise Exception("Unable to refresh credentials")
     return creds
+
+def build_service():
+    creds = get_authenticated_client()
+    service = build('gmail', 'v1', credentials=creds)
+    return service
--- app/email.py
+++ app/email.py
@@ -1,60 +1,35 @@
 from threading import Thread
-from . import mail
-from flask_mail import Message
-from flask import current_app, render_template
-from .auth.oauth2 import get_authenticated_client
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+from flask import current_app, render_template
+from .auth.oauth2 import build_service
+import base64
+from email.mime.text import MIMEText
 
 def send_async_email(app, message):
     with app.app_context():
         mail.send(message)
 
 def send_email(to, subject, template, **kwargs):
-    creds = get_authenticated_client()
+    service = build_service()
     app = current_app._get_current_object()
-    msg = Message(app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['WECHE_MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
-
-    # Build the Gmail service
-    service = build('gmail', 'v1', credentials=creds)
-
-    # Create a message object
-    raw_message = create_message(app.config['WECHE_MAIL_SENDER'], to, subject, msg.html, msg.body)
-
-    # Send the email using the Gmail API
-    send_message(service, 'me', raw_message)
-
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
-def create_message(sender, to, subject, html, text):
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    import base64
-
-    message = MIMEMultipart('alternative')
-    message['Subject'] = subject
-    message['From'] = sender
-    message['To'] = to
-
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
-
-    message.attach(part1)
-    message.attach(part2)
-
-    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    return {'raw': raw_message}
+    msg = MIMEText(render_template(template + '.html', **kwargs), 'html')
+    msg['to'] = to
+    msg['from'] = app.config['WECHE_MAIL_SENDER']
+    msg['subject'] = app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' ' + subject
+    raw = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+    raw_message = {'raw': raw}
+    try:
+        send_message(service, 'me', raw_message)
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        raise Exception(f'An error occurred: {error}')
 
 def send_message(service, user_id, message):
     try:
         message = service.users().messages().send(userId=user_id, body=message).execute()
         print(f'Message Id: {message["id"]}')
         return message
-    except Exception as error:
+    except HttpError as error:
         print(f'An error occurred: {error}')
-        raise
+        raise Exception(f'An error occurred: {error}')
