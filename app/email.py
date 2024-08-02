ce7c2f7411d92c127ec4d09af27d3ac3f6c00b7f
Wacoder Forever <sethomondi088@gmail.com>
2024-08-02 23:29:10 +0300

    hvjghjg

--- app/email.py
+++ app/email.py
@@ -1,35 +1,13 @@
 from threading import Thread
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from flask import current_app, render_template
-from .auth.oauth2 import build_service
-import base64
-from email.mime.text import MIMEText
+from . import mail
+from flask_mail import Message
+from flask import current_app
 
-def send_async_email(app, message):
+def send_async_email(app,message):
     with app.app_context():
-        mail.send(message)
+        mail.send(msg)
 
-def send_email(to, subject, template, **kwargs):
-    service = build_service()
-    app = current_app._get_current_object()
-    msg = MIMEText(render_template(template + '.html', **kwargs), 'html')
-    msg['to'] = to
-    msg['from'] = app.config['WECHE_MAIL_SENDER']
-    msg['subject'] = app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' ' + subject
-    raw = base64.urlsafe_b64encode(msg.as_bytes()).decode()
-    raw_message = {'raw': raw}
-    try:
-        send_message(service, 'me', raw_message)
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-        raise Exception(f'An error occurred: {error}')
-
-def send_message(service, user_id, message):
-    try:
-        message = service.users().messages().send(userId=user_id, body=message).execute()
-        print(f'Message Id: {message["id"]}')
-        return message
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-        raise Exception(f'An error occurred: {error}')
+def send_email(to,subject,template,**kwargs):
+    app=current_app._get_current_object()
+    msg=Message(app.config['WECHE_MAIL_SUBJECT_PREFIX'] + ' '+ subject,
+                sender=app.config['WECHE_MAIL_SENDER'])
