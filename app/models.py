834267b11ee3dd6d8768187bcd78ef9aecef6281
Wacoder Forever <sethomondi088@gmail.com>
2024-08-06 04:55:47 +0300

    gfghg

--- app/models.py
+++ app/models.py
@@ -74,3 +74,24 @@ class Permission:
     WRITE_ARTICLE=0x04
     MODERATE=0x08
     ADMIN=0x80
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+        'User': (Permission.FOLLOW |
+                Permission.COMMENT |
+                Permission.WRITE_ARTICLES, True),
+        'Moderator': (Permission.FOLLOW |
+                    Permission.COMMENT |
+                    Permission.WRITE_ARTICLES |
+                    Permission.MODERATE_COMMENTS, False),
+        'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
