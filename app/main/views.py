98c1a751addbb15facb05c2c621100d8dcc65f89
Wacoder Forever <sethomondi088@gmail.com>
2024-08-08 01:11:05 +0300

    hgjghjg

--- app/main/views.py
+++ app/main/views.py
@@ -33,3 +33,10 @@ def index():
 
     return render_template('index.html', current_time=datetime.utcnow(), name=session.get('name'),
                            form=form, known=session.get('known', False))
+
+@main.route('/user/<username>')
+def user(username):
+    user=User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html',user=user)
